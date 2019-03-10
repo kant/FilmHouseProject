c32af13d75cd21606d1cee111f0ca10725b5e5c5
Ptipiak <ptipiak.off@gmail.com>
2019-03-10 22:51:17 +0000

    Fixing the architechture

--- public/js/second.js
+++
@@ -1,7 +0,0 @@
-// Require ipcRenderer
-const { ipcRenderer } = require('electron');
-
-ipcRenderer.on('update-from-mainWindow', (event, data) => {
-  console.log(data.value)
-  document.getElementById('DisplayData').innerHTML = data.value
-})
