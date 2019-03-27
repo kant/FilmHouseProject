02eefe46d5e32840f185909af002b8f901ad37a6
Ptipiak <ptipiak.off@gmail.com>
2019-03-27 13:03:17 +0000

    Working on the front-view and info display

--- app/public/js/slideShow.js
+++ app/public/js/slideShow.js
@@ -22,11 +22,15 @@ function showSlides() {
 
 ipcRenderer.on('update-from-mainWindow', (event, data) => {
   var titles = document.getElementsByClassName("title")
+  var hour = document.getElementsByClassName("hour")
+  var cert = document.getElementsByClassName("cert")
   console.log(data)
   for(var i = 0; i < data.length; i++) {
     if(i < titles.length) {
       console.log(titles[i])
       titles[i].innerHTML = data[i].info.API_static_MainTitle
+      hour[i].innerHTML = data[i].data.API_static_Showtime      
+      cert[i].innerHTML = data[i].info.API_static_Cert
     }
   }
 })
