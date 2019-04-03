156f0b2166931d07b851aef7758a4aae5161eff4
Ptipiak <ptipiak.off@gmail.com>
2019-04-03 09:08:50 +0100

    working on the view

--- app/public/js/slideShow.js
+++ app/public/js/slideShow.js
@@ -21,16 +21,24 @@ function showSlides() {
   setTimeout(showSlides, 4000); // Change image every 10 seconds
 }
 
+const slide_html = "<divclass=\"mySlidesfade\"><divclass=\"numbertext\">1/3</div><divclass=\"master-container\"><articleclass=\"parent-container\"><divclass=\"content-wrapper\"><divclass=\"image-wrapper\"><imgsrc=\"https://www.filmhousecinema.com/sites/filmhousecinema.com/files/styles/extra_large/public/shows/2019/TheAftermath02.jpg\"></div><divclass=\"text-wrapper\"><ulclass=\"data-display\"><ul></div></div></article></div></div>"
+
 function RenderNewMovie(data) {
-  // console.log(data)
-  // data.map(e => {
-  //   console.log(e)
-  // })
+  console.log(slide_html)
+  var container = document.querySelector('.slideshow-container')
+  data.map(e => {
+    container.append(slide_html)
+    var display = document.querySelector('.data-display')
+    for(i of e) {
+      var node = document.createElement("LI");
+      node.innerHTML = i
+    }
+  })
 }
 
 ipcRenderer.on('data-update', (event, data) => {
   console.log(data)
-  // for(var i = 0; i < data.length; i++) {
-  //   RenderNewMovie(data)
-  // }
+  for(var i = 0; i < data.length; i++) {
+    RenderNewMovie(data)
+  }
 })
